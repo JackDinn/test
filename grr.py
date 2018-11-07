3541e43a84dfdef97735bfd5804c69285a98ac3a
greg <jack@jackdinn.co.uk>
2018-11-07 17:29:02 +0000

    hi

--- grr.py
+++ grr.py
@@ -4,6 +4,7 @@ x = ['hello', 'buy']
 print(x[0])
 print(x)
 m = re.search('(?<=-)\w+', 'spam-egg')
-print()
-for f in range(1, 10):
-    print(f)
+print(m.group(0))
+# for f in range(1, 10):
+    # print(f)
+

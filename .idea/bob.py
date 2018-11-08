944b6127dfe91fd3121ba938b2c6d3ab05333a34
greg <jack@jackdinn.co.uk>
2018-11-08 22:22:44 +0000

    hi

--- .idea/bob.py
+++
@@ -1,7 +0,0 @@
-import re
-print('hi')
-x = ['hello', 'buy']
-print(x[0])
-print(x)
-m = re.search('(?<=-)\w+', 'spam-egg')
-print()
--- grr.py
+++ grr.py
@@ -6,12 +6,13 @@ print(x[0])
 print(x)
 m = re.search('(?<=-)\w+', 'spam-egg')
 print(m.group(0))
-# for f in range(1, 10):
-    # print(f)
+for hjj in range(1, 10, ):
+    print(hjj)
 
 print('test')
 
 
 print('change 18:41')
 print('branch')
-print('branch2')P
+print('branch2')
+print('branch3')
